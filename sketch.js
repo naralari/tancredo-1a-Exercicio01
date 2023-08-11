2ade41d90f71e1f51617b72e0aa7f0c7c8a9a206
Lari silva <141414702+naralari@users.noreply.github.com>
2023-08-11 08:18:18 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 600);
+   background("black");
+ 
+ 
+}
+
+function draw() {
+ 
+  fill("purple");
+  stroke("whithe");
+ 
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,30,30)
+   
+  }
+ 
+ 
+}
